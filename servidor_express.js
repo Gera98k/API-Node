e4b706c6cb3ad10e7aa27579e7f54fadfaeeb752
gera98k <apariciog126@gmail.com>
2026-02-17 21:51:42 -0600

    Implementacion de un middleware sencillo

--- servidor_express.js
+++ servidor_express.js
@@ -5,6 +5,25 @@ const ditto = require('./ditto.json');
 app.disable('x-powered-by');
 const PORT = process.env.PORT ?? 1234;
 
+app.use((req, res, next) => {
+    console.log('mi primer middleware');
+    if(req.method === 'POST') return next();
+    if(req.headers['content-type'] === 'application/json') return next();
+
+    let body = '';
+
+    req.on('data', chunk => {
+        body += chunk.toString();
+    });
+
+    req.on('end', chunk => {
+        const data = JSON.parse(body);
+        data.timestamp = Date.now();
+        req.body = data;
+        next();
+    });
+});
+
 app.get('/pokemon/ditto', (req, res) => {
     res.json(ditto);
 });
