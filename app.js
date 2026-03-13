319b1f104b4044af6996b5d73606f917c8cd5fd6
gera98k <apariciog126@gmail.com>
2026-03-12 23:50:11 -0600

    encontrar pelicula por id

--- app.js
+++ app.js
@@ -3,12 +3,20 @@ const peliculas = require('./peliculas.json');
 const app = express();
 app.disable('x-powered-by');
 
-app.get('/', (req, res) => {
-    res.json({ message: 'hola mundo' });
-});
-
 app.get('/peliculas', (req, res) => {
     res.json(peliculas);
+});
+
+app.get('/peliculas/:id', (req, res) => {
+    const { id } = req.params;
+    const pelicula = peliculas.find(peli => 
+        peli.id === id
+    );
+
+    if(pelicula){
+        return res.json(pelicula);
+    }
+    res.status(404).json({ message: 'pelicula no encontrada' });
 });
 
 const PORT = process.env.PORT ?? 3000;
