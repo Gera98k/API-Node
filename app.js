abb03cd88876326e249da14ff6e5e4583ca19a7d
gera98k <apariciog126@gmail.com>
2026-03-20 00:25:22 -0600

    Agregar peliculas nuevas con comprobaciones de zod y un schema separado

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const peliculas = require('./peliculas.json');
+const crypto = require('node:crypto');
+const { validarPeli } = require('./schemas/peliculas');
 const app = express();
+app.use(express.json()); // express.json() es el middleware de express para tratar datos y chunks de un POST
 app.disable('x-powered-by');
 
 //filtrar pelicula por id
@@ -30,6 +33,23 @@ app.get('/peliculas', (req, res) => {
         return res.json(peliculas);
     }
     res.status(404).json({ message: 'error' });
+});
+
+app.post('/peliculas', (req, res) => {
+
+    const resultado = validarPeli(req.body);
+
+    if(!resultado.success) {
+        return res.status(400).json({ error: JSON.parse(resultado.error.message)});
+    }
+
+    const nuevaPeli = {
+        id: crypto.randomUUID(),
+        ...resultado.data
+    }
+    
+    peliculas.push(nuevaPeli);
+    res.status(201).json(nuevaPeli);
 });
 
 const PORT = process.env.PORT ?? 3000;
---
+++ schemas/peliculas.js
@@ -0,0 +1,30 @@
+const z = require('zod');
+
+const schemaPeli = z.object({
+    title: z.string({
+        invalid_type_error: 'El titulo debe ser string',
+        required_error: 'El titulo es un campo requerido'
+    }),
+    year: z.number().int().min(1900).max(2027),
+    director: z.string(),
+    duration: z.number().int().positive(),
+    rate: z.number().min(0).max(10).default(5),
+    poster: z.string().url({
+        message: 'La portada es invalida'
+    }),
+    genre: z.array(
+        z.enum(['Action', 'Adventure', 'Crime', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Thriller', 'Sci-Fi']),
+        {
+            required_error: 'El genero es obligatorio',
+            invalid_type_error: 'El genero debe ser un array'
+        }
+    )
+});
+
+function validarPeli(input){
+    return schemaPeli.safeParse(input);
+}
+
+module.exports = {
+    validarPeli
+}
