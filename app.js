dbca2930da03866e061eb8128133a5200988195f
gera98k <apariciog126@gmail.com>
2026-03-26 22:47:27 -0600

    Implementar el metodo DELETE para eliminar peliculas desde el frontend habilitando el CORS para metodos simples y complejos

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const app = express();
 app.use(express.json()); // express.json() es el middleware de express para tratar datos y chunks de un POST
 app.disable('x-powered-by');
 
+//origenes que el CORS aceptara
+const ORIGENES_ACEPTADOS = [
+    'http://localhost:8080'
+];
+
 //filtrar pelicula por id
 app.get('/peliculas/:id', (req, res) => {
     const { id } = req.params;
@@ -21,6 +26,13 @@ app.get('/peliculas/:id', (req, res) => {
 
 //filtrar peliculas por genero
 app.get('/peliculas', (req, res) => {
+    //metodos simples como GET no necesitan el OPTIONS para hacer funcionar el CORS
+    const origen = req.header('origin');
+
+    if(ORIGENES_ACEPTADOS.includes(origen) || !origen) {
+        res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+    }
+
     const { genre } = req.query;
     if(genre) {
         console.log(genre);
@@ -35,6 +47,8 @@ app.get('/peliculas', (req, res) => {
     res.status(404).json({ message: 'error' });
 });
 
+
+//Agregar pelicula nueva validando completamente con zod
 app.post('/peliculas', (req, res) => {
 
     const resultado = validarPeli(req.body);
@@ -53,7 +67,7 @@ app.post('/peliculas', (req, res) => {
 });
 
 
-//Actualizar pelicula
+//Actualizar pelicula validando parcialmente con zod
 app.patch('/peliculas/:id', (req, res) => {
     const resultado = validarPeliculaParcialmente(req.body);
 
@@ -76,6 +90,36 @@ app.patch('/peliculas/:id', (req, res) => {
     peliculas[indexPelicula] = actualizarPelicula;
 
     return res.json(actualizarPelicula);
+});
+
+//Eliminar pelicula por id
+app.delete('/peliculas/:id', (req, res) => {
+    const origen = req.header('origin');
+    if(ORIGENES_ACEPTADOS.includes(origen) || !origen) {
+        res.header('Access-Control-Allow-Origin', origen);
+    }
+
+    const { id } = req.params;
+    const indexPelicula = peliculas.findIndex(movie => movie.id === id );
+
+    if(indexPelicula === -1){
+        return res.status(401).json({ message: 'Pelicula no encontrada' });
+    }
+
+    peliculas.splice(indexPelicula, 1);
+
+    return res.json({ message: 'Pelicula borrada' });
+});
+
+
+//metodos como DELETE necesitan pasarle options para el CORS
+app.options('/peliculas/:id', (req, res) => {
+    const origen = req.header('origin'); 
+    if(ORIGENES_ACEPTADOS.includes(origen) || !origen) {
+        res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    }
+    res.send(200);
 });
 
 const PORT = process.env.PORT ?? 3000;
